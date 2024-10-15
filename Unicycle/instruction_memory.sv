0c00fda6eee11678d53980c5097ea3e9d48189da
MarinGE23 <126499377+MarinGE23@users.noreply.github.com>
2024-10-15 02:55:13 -0600

    Add instruction memory and unicycle draft

---
+++ Unicycle/instruction_memory.sv
@@ -0,0 +1,29 @@
+module instruction_memory #(
+    parameter DATA_WIDTH = 20,
+    parameter ADDRESS_WIDTH = 8,
+    parameter MEM_SIZE = 256
+) (
+    input logic clk,                             // Clock 
+    input logic [ADDRESS_WIDTH-1:0] address,     // Input address to read instruction
+    output logic [DATA_WIDTH-1:0] instruction    // Output instruction read from memory
+);
+
+    // Array to store instructions
+    reg [DATA_WIDTH-1:0] instruction_memory [0:MEM_SIZE-1];
+
+    // Initialize memory with content from file
+    initial begin
+		  $readmemh("program.hex", instruction_memory);
+    end
+
+    // Assign instruction based on address
+    always @(*) begin
+        if (address >= 0 && address < MEM_SIZE) begin
+            // Read instruction from memory array
+            instruction = instruction_memory[address];
+        end else begin
+            instruction = {DATA_WIDTH{1'bx}}; // Undefined value in case of invalid address
+        end
+    end
+
+endmodule
---
+++ Unicycle/instruction_memory_tb.sv
@@ -0,0 +1,37 @@
+module instruction_memory_tb;
+	 localparam DATA_WIDTH = 20;
+    localparam ADDRESS_WIDTH = 8;
+    localparam MEM_SIZE = 256;
+
+    // Signal declaration
+    reg clk = 0;
+    reg [ADDRESS_WIDTH-1:0] address;
+    reg [DATA_WIDTH-1:0] instruction;
+
+    // Instantiate the instruction_memory module
+    instruction_memory #(
+        DATA_WIDTH, 
+        ADDRESS_WIDTH, 
+        MEM_SIZE
+    ) dut (
+        .clk(clk),
+        .address(address),
+        .instruction(instruction)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        #5;
+        // Read first 10 instructions from instruction_memory
+        for (int i = 0; i < 10; i++) begin
+            address = i;
+            #10;
+            $display("Instruction%d: %h", i+1, instruction);
+        end
+
+        $display("Testbench completed successfully");
+        $finish;
+    end
+
+endmodule
