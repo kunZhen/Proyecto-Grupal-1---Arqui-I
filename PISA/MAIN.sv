18e93f5f36c38cb48d0f4ab674989f86eb2d5110
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-10-19 22:30:13 -0600

    [UPDATE] .gitignore

--- PISA/MAIN.sv
+++ PISA/MAIN.sv
@@ -11,10 +11,14 @@ module MAIN (
    output logic [7:0] green,
    output logic [7:0] blue,
 	
-	input logic wren,
-	input logic [31:0] data,       
-	output [7:0] q, 
-	output [17:0] rdaddress, wraddress
+	input logic wren_a,
+	input logic wren_b,
+	input logic [31:0] data_a,       
+	input logic [31:0] data_b, 
+	output logic [31:0] q_a, 
+	output logic [31:0] q_b,
+	output logic [16:0] address_a, address_b, 
+	output logic [3:0] byteena_a, byteena_b
 	
 );
 
@@ -32,11 +36,17 @@ module MAIN (
 		.red(red),                // Salida de color rojo (8 bits)
 		.green(green),            // Salida de color verde (8 bits)
 		.blue(blue),              // Salida de color azul (8 bits)
-		.wren(wren),
-		.data(data),
-		.q(q),
-		.rdaddress(rdaddress),
-		.wraddress(wraddress)
+		
+		.address_a(address_a),
+      .address_b(address_b),
+      .byteena_a(byteena_a),
+      .byteena_b(byteena_b),
+      .data_a(data_a),
+      .data_b(data_b),
+      .wren_a(wren_a),
+      .wren_b(wren_b),
+      .q_a(q_a),
+      .q_b(q_b)
 	);
 
 endmodule
