5aa8b13d70708abfefb22743e9e400e9e2440011
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-10-21 12:38:28 -0600

    [DELETE] unicycle

--- Unicycle/mux2.sv
+++
@@ -1,8 +0,0 @@
-module mux2 #(parameter WIDTH = 20) (
-    input logic sel,
-    input logic [WIDTH-1:0] a, b,
-    output logic [WIDTH-1:0] y
-);
-	assign y = sel ? b : a;
-
-endmodule
--- Unicycle/pisa_test3.asm
+++
@@ -1,6 +0,0 @@
-# Set 0x3F
-nop
-addip s9, zero, 3
-sllip s9, s9, 4				# s9 = 0x30
-addip t2, zero, 15
-orp s9, s9, t2				# s9 = 0x3F
